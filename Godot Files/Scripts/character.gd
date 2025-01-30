184b64e2c2bc4192d1713ff11a90af229a66a4e3
Liam S <128020513+LSUon@users.noreply.github.com>
2025-01-30 20:47:15 +1100

    Removed old comments
    Removed old comments from the player script (can find them in git later anyway)

--- Godot Files/Scripts/character.gd
+++ Godot Files/Scripts/character.gd
@@ -26,12 +26,7 @@ func getHangingOnWall() -> bool:
 		for index in get_slide_collision_count():
 			var collision := get_slide_collision(index)
 			var collider = collision.get_collider()
-			# One way of doing collision detection I removed for a more universal approach
-			#if collider is TileMap:
-				#var tile_rid = get_slide_collision(index).get_collider_rid()
-				#var layer = PhysicsServer2D.body_get_collision_layer(tile_rid)
-				#if layer == 1:
-					#return true
+
 			if collider is TileMapLayer:
 				var tile_map:TileMapLayer = collider
 				var hit_cell = tile_map.local_to_map(collision.get_position() + collision.get_normal() * - 4)
@@ -52,8 +47,6 @@ func _physics_process(delta: float) -> void:
 
 	if onWall:
 		velocity.y = 0
-		
-
 		
 	var upDown := Input.get_axis("Up", "Down")
 	if onWall:
@@ -83,77 +76,11 @@ func _physics_process(delta: float) -> void:
 		await get_tree().create_timer(0.1).timeout
 		set_collision_mask_value(2,true)
 		
-		# Keeping this around to show of the stupid stuff I was doing :)
-		#var space_state = get_world_2d().direct_space_state
-		#var query = PhysicsRayQueryParameters2D.create(global_position, global_position + Vector2.DOWN * 100,
-			#collision_mask, [self])
-		#var results := space_state.intersect_ray(query)
-		#print(results)
-		##print(results.values().back())
-		#if results:
-			#var collider:Node2D = results["collider"]
-			#print(collider.get_groups())
-			#if (collider.is_in_group("Platforms")):
-				#print("it's a platform!")
-				#var coll:StaticBody2D = collider
-				#coll.set_collision_layer_value(1,false)
-				#await get_tree().create_timer(0.1).timeout
-				#coll.set_collision_layer_value(1,true)
-
-		#var closest := results.values().back()
-		#for result in results.values():
-			#if len(result.position - position) < len(closest.position - position):
-				#closest = result
-		
-
-	
-		
 	# Dash Mechanic
 	if Input.is_action_just_pressed("Dash"):
 		dashDir = facing
 		velocity.x = DASH_SPEED * dashDir
 		start_dash = Time.get_ticks_msec()
-	# First attempt at a "grab" dash mechanic... I think we should replace this by sending out an scene rather than just a raycast (which is also buggy)
-	#if Input.is_action_just_pressed("Dash"):
-		#dashDir = facing
-		##velocity.x = DASH_SPEED * dashDir
-		##start_dash = Time.get_ticks_msec()
-		#var space_state = get_world_2d().direct_space_state
-		#var mask = 0b00000000_00000000_00000000_00000100 # Only look on environment layer
-		#var queryMiddle = PhysicsRayQueryParameters2D.create(global_position, global_position + Vector2.RIGHT * 500 * dashDir, mask, [self])
-		##var shape:RectangleShape2D = $CollisionShape2D.shape
-		##shape.size.x
-		##var queryTop = PhysicsRayQueryParameters2D.create(global_position + Vector2.UP * ($CollisionShape2D.shape.size.y/2), global_position + Vector2.UP * ($CollisionShape2D.shape.size.y/2) + Vector2.RIGHT * 500 * dashDir, mask, [self])
-		##var queryBottom = PhysicsRayQueryParameters2D.create(global_position + Vector2.UP * -($CollisionShape2D.shape.size.y/2), global_position + Vector2.UP * -($CollisionShape2D.shape.size.y/2) + Vector2.RIGHT * 500 * dashDir, mask, [self])
-		#var results := space_state.intersect_ray(queryMiddle)
-		#if results:
-			#print(results)
-			#var pos:Vector2 = results["position"]
-			#
-			#start_grab = Time.get_ticks_msec()
-			#grab_start_pos = position
-			#grabPosition = pos
-			#velocity.y = 0
-			#calculated_grab_time = abs(grabPosition.x - grab_start_pos.x) / GRAB_SPEED
-			#var pos = results[""]
-			#var collider:Node2D = results["collider"]
-			#print(collider.get_groups())
-			#if (collider.is_in_group("Platforms")):
-				#print("it's a platform!")
-				#var coll:StaticBody2D = collider
-				#coll.set_collision_layer_value(1,false)
-				#await get_tree().create_timer(0.1).timeout
-				#coll.set_collision_layer_value(1,true)
-	#if (Time.get_ticks_msec() - start_grab <= calculated_grab_time) and start_grab != 0:
-		#velocity.x = lerp(grab_start_pos.x,grabPosition.x,(Time.get_ticks_msec() - start_grab) /calculated_grab_time)
-		##print("Lerp: " + String(lerp(grab_start_pos.x,grabPosition.x,(Time.get_ticks_msec() - start_grab) /5000)) + ", GrabStart: " + String(grab_start_pos.x) + ", GrabPosition: " + String(grabPosition.x) )
-		#print("start")
-		#print(calculated_grab_time)
-		#print(lerp(grab_start_pos.x,grabPosition.x,(Time.get_ticks_msec() - start_grab) /500.0))
-		#print(grab_start_pos)
-		#print(grabPosition)
-		#print((Time.get_ticks_msec() - start_grab) /500.0)
-		#print(position)
 		
 	if (Time.get_ticks_msec() - start_dash < 150):
 		velocity.x = lerp(DASH_SPEED*dashDir,0.0, float(Time.get_ticks_msec() - start_dash)/500.00)
